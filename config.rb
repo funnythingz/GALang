b246614c3a20037f47beffd82d76769529053024
Author funnythingz <hiroki@hackleberry.me>
2014-07-11 10:31:12 +0900

    init

---
+++ config.rb
@@ -0,0 +1,8 @@
+http_path = "/"
+css_dir = "dist/"
+sass_dir = "src/scss"
+images_dir = "dist/imgs"
+javascripts_dir = "dist/"
+
+output_style = :compressed
+line_comments = false
---
+++ src/ts/_simple-controller.ts
@@ -0,0 +1,15 @@
+module App {
+
+    export interface ISimpleScope extends ng.IScope {
+        hoge: string;
+    }
+
+    export class SimpleController {
+
+        constructor(private $scope: ISimpleScope) {
+
+        }
+
+    }
+
+}
